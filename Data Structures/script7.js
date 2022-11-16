8a151b3f57ead444114042ae1f2e178c7b532d9c
Samuel <82509653+samuel-ma@users.noreply.github.com>
2022-11-16 14:00:14 +0200

    Create script7.js

---
+++ Data Structures/script7.js
@@ -0,0 +1,19 @@
+
+
+let words = "The quick brown fox jumped over the lazy dog";
+
+function findLongestWordLength(str) {
+  let words = str.split(' ');
+  let maxLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
+
+findLongestWordLength(words);
+
