4ddd437ff629d9d5b910b64da4b481fc5a9ea7a6
Samuel <82509653+samuel-ma@users.noreply.github.com>
2022-11-16 16:39:04 +0200

    More on Algorithms

--- Data Structures/script8.js
+++ Data Structures/script8.js
@@ -1,14 +1,19 @@
 
 
 //find the largest number in an array
-function largestOfFour(arr) {
-  let newArr = [];
 
-  for(let i=0; i<arr.length; i++){
-    let largestNum = Math.max.apply(null, arr[i]);
-    newArr.push(largestNum);
-  }
-  return newArr;
+let myArr = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
+console.log(myArr);
+let arr = [3,1,5];
+
+// console.log(Math.max.apply(null, arr));
+
+let newArr = [];
+
+for(let i=0; i<myArr.length; i++){
+
+        let finalValue = Math.max.apply(null, myArr[i]);
+        newArr.push(finalValue);
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+console.log(newArr);
