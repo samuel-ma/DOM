960876dba7d5a0d750769e2875e1cbea829e2eb7
Samuel <82509653+samuel-ma@users.noreply.github.com>
2022-11-16 14:22:00 +0200

    Update script7.js

--- Data Structures/script7.js
+++ Data Structures/script7.js
@@ -1,19 +1,19 @@
 //Find the longest word in a String
 
-let words = "The quick brown fox jumped over the lazy dog";
+let myWords = "The quick brown fox jumped over the lazy dog";
 
-function findLongestWordLength(str) {
-  let words = str.split(' ');
-  let maxLength = 0;
+function longestWord(str){
+    let words = str.split(" ");
+    let maxLength = 0;
 
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].length > maxLength) {
-      maxLength = words[i].length;
+    for (let i=0; i<words.length; i++){
+        if(words[i].length > maxLength){
+            maxLength = words[i].length;
+        }
     }
-  }
-
-  return maxLength;
+    return maxLength;
 }
 
-findLongestWordLength(words);
+console.log(`The Longest word in the string is ${longestWord(myWords)} words long.`);
+
 
