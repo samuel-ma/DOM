64a6ea2155fb3ef4089e9d28c0fe88dd860070b2
Samuel <82509653+samuel-ma@users.noreply.github.com>
2022-11-21 18:48:45 +0200

    Working with objects

--- Data Structures/script2.1.js
+++ Data Structures/script2.1.js
@@ -3,69 +3,47 @@
 //SOLUTION USING HASH TABLES
 const obj1 = {
     arr1: [3,1,7,9]
-}
+  };
 
 const obj2 = {
     arr2: [2,4,1,9,3]
-}
+  };
 
-const joinedObj = {...obj1, ...obj2};
-console.log(`Joined two objects: ${joinedObj}`);
+let value1 = Object.values(obj1);
+let finalV1 = value1[0];
+// console.log(finalV1);
 
-const finalObj = {
-    3: 2,
-    1: 2,
-    7: 1,
-    9: 2,
-    2: 1,
-    4: 1
-}
+let value2 = Object.values(obj2);
+let finalV2 = value2[0];
+// console.log(finalV2);
 
-const newArr = [];
-
-let finalArr = Object.entries(finalObj);
-console.log(`Object entries: ${finalArr}`);
-let keyArr = Object.keys(finalObj);
-console.log(`Object keys: ${keyArr}`);
-let valueArr = Object.values(finalObj);
-console.log(`Object values: ${valueArr}`);
-
-//convert the string arrays to number arrays
-let numberArr = keyArr.map(Number);
-console.log(`Object keys converted to number arrays: ${numberArr}`);
-
-//sum of all the keys
-let count = 0;
-let finalSum;
-
-for(let i=0; i<numberArr.length; i++){
-    count += numberArr[i];
-    finalSum = count;
-}
-console.log(`Sum of all keys: ${finalSum}`);
+//single array of both elements
+let joinedArr = finalV1.concat(finalV2);
+// console.log(joinedArr);
 
 
 
-//find the sum of all values that are recurring
-for(let i=0; i<finalArr.length; i++){
-    let sum;
-    let finalCount;
-    let count;
-
-    if(valueArr === 2 ){
-        count = numberArr * 2;
-        sum += count;
-        finalCount = sum;
-
-        console.log(sum);
-        console.log(finalCount);
-
-    } else if(valueArr == 1){
-        count = numberArr;
-        sum += count;
-        finalCount = sum;
-
-        console.log(finalCount);
-
+// find the distinct values
+function compareArrays(arrr1, arrr2){
+    let finalArr = arrr2.filter(function(obj){
+    return arrr1.indexOf(obj) === -1;
+})
+    console.log("The overlapping elements are", finalArr);
+    
+    //sum up the entire array
+    let sum = 0;
+    for(let i=0; i<finalArr.length; i++){
+        sum += finalArr[i];
     }
+    
+    let finalSum = sum;
+    // console.log(finalSum);
+    return finalSum;
 }
+
+let finalSum1 = compareArrays(finalV1, finalV2);
+let finalSum2 = compareArrays(finalV2, finalV1);
+
+//calculate the finalSum
+let finalAmount = finalSum1 + finalSum2;
+console.log("The sum of overlapping elements is", finalAmount);
