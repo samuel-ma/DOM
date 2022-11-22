8ac1ae13c22ab8d998cc6bd4b154e1e1c84e97e3
Samuel <82509653+samuel-ma@users.noreply.github.com>
2022-11-22 20:34:44 +0200

    Update script.js

--- Data Structures/script.js
+++ Data Structures/script.js
@@ -1,9 +1,9 @@
-"use strict";
+    "use strict";
 
-//PROBLEM 1
-//FIRST SOLUTION USING (ARRAYS)
-const array1 = [3,1,7,9];
-const array2 = [2,4,1,9,3];
+    //PROBLEM 1
+    //FIRST SOLUTION USING (ARRAYS)
+    const array1 = [3,1,7,9];
+    const array2 = [2,4,1,9,3];
 
     console.log("PROBLEM 1")
     console.log("SOLUTION USING ARRAYS");
@@ -12,12 +12,12 @@ const array2 = [2,4,1,9,3];
     function distinctElements(arr1, arr2){
         let finalFilter;
 
-        //filter through the array and find the distinct elements
-        const output = arr2.filter(function (obj) {
-            finalFilter = arr1.indexOf(obj) === -1;
-            return finalFilter;
-        });
-        console.log("The distinct elements are", output);
+    //filter through the array and find the distinct elements
+    const output = arr2.filter(function (obj) {
+         finalFilter = arr1.indexOf(obj) === -1;
+    return finalFilter;
+    });
+    console.log("The distinct elements are", output);
     
     //sum of all elements inside the new filteredArray
     let sum = 0;
@@ -30,9 +30,8 @@ const array2 = [2,4,1,9,3];
     
     // console.log(distinctElements(array1, array2));
     // console.log(distinctElements(array2, array1));
-    
+
     let finalOutput = (distinctElements(array1, array2) + distinctElements(array2, array1));
-    
     console.log("The final sum is", finalOutput);
     
     
